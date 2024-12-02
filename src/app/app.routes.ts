d59803eb67c11262677777f6ade74e31dc597942
Aletdev <alejandrortsinani@outlook.com>
2024-12-02 17:06:33 -0400

    Scaffolding inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { StoresComponent } from './components/stores/stores.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { DetailsOrdersComponent } from './components/details-orders/details-orders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'stores', 
+        component: StoresComponent
+    },
+    { 
+        path: 'products', 
+        component: ProductsComponent
+    },
+    { 
+        path: 'categories', 
+        component: CategoriesComponent
+    },
+    { 
+        path: 'orders', 
+        component: OrdersComponent
+    },
+    { 
+        path: 'details-orders', 
+        component: DetailsOrdersComponent
+    },
+    { 
+        path: '', 
+        redirectTo: '/stores', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: '**', 
+        redirectTo: '/stores' 
+    },
+];
---
+++ src/app/components/details-orders/details-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-orders.component.html',
+  styleUrl: './details-orders.component.css'
+})
+export class DetailsOrdersComponent {
+
+}
